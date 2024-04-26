56ac17041f8b84b157d8ae25544e795cf2ccdb9d
DuffyLuscyh <109970046+DuffyLuscyh@users.noreply.github.com>
2024-04-26 11:11:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual = new Date();
-let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 12-03-2025");
+let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2025-05-12");
 dataObjetivo = new Date(dataObjetivo+"T23:59:59");
 let diasQueFaltam = Math.floor((dataObjetivo - dataAtual) / 86400000);
 document.querySelector("#dias_restantes").textContent = diasQueFaltam+" dias";
