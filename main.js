266f4056b4fe801bdded45d9616272aae3c799a4
DuffyLuscyh <109970046+DuffyLuscyh@users.noreply.github.com>
2024-05-10 00:12:05 -0300

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-const dataAtual = new Date();
-let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2024-05-14");
-dataObjetivo = new Date(dataObjetivo+"T23:59:59");
-let diasQueFaltam = Math.floor((dataObjetivo - dataAtual) / 86400000);
-document.querySelector("#dias_restantes").textContent = diasQueFaltam+" dias";
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  const progressBar = card.querySelector('.progress-bar');
+  const progressInfo = card.querySelector('.info span:first-child');
+
+  const days = parseInt(progressInfo.innerText.split(' ')[0]);
+  const hours = parseInt(progressInfo.innerText.split(' ')[2]);
+  const minutes = parseInt(progressInfo.innerText.split(' ')[4]);
+
+  const totalSeconds = (days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60);
+
+  const updateProgressBar = () => {
+    const now = new Date();
+    const then = new Date(now.getFullYear(), 0, 1);
+    const secondsElapsed = Math.floor((now - then) / 1000);
+    const percentageComplete = (secondsElapsed / totalSeconds) * 100;
+
+    progressBar.style.width = `${percentageComplete}%`;
+
+    if (percentageComplete === 100) {
+      clearInterval(intervalId);
+    }
+  };
+
+  let intervalId = setInterval(updateProgressBar, 1000);
+});
